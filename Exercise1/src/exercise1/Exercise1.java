b342867bbcc80fc791b394d8b4b9fbffd23acafc
sgonzalez <sgonzalez@106-03.info2>
2024-09-20 13:44:19 +0200

    Oh shit, here were go again

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,13 @@
+package exercise1;
+
+public class Exercise1 {
+
+public static void main(String[] args) {
+	
+	System.out.print("hello, how you are?");
+	System.out.println("Reventao primo");
+	
+}
+
+}
+
